4f7a660d1b540768498f280c5a624760f01fdd50
bawumbila <74751215+bawumbila@users.noreply.github.com>
2024-01-21 20:38:43 -0500

    grosery app changes

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,38 @@
 // const express = require('express');
 // const router = express.Router();
 
-
 // router.get('/', function(req, res, next) {
 //     res.render("index");
 //   });
-  
+
 //   module.exports = router;
 
+const express = require("express");
+const router = express.Router();
+const groceryListCtrl = require("../controllers/groceryList");
 
+router.get("/:id/edit", groceryListCtrl.edit);
+router.get("/:id/grocerylistedit", groceryListCtrl.editgrocerylist);
 
-  const express = require('express');
-  const router = express.Router();
-  const groceryListCtrl = require('../controllers/groceryList');
+router.get("/", groceryListCtrl.index);
+router.get("/new", groceryListCtrl.new);
 
-  router.get('/:id/edit', groceryListCtrl.edit);
-  router.get('/:id/grocerylistedit', groceryListCtrl.editgrocerylist);
-  
-  router.get('/', groceryListCtrl.index);
-  router.get('/new', groceryListCtrl.new);
-  router.post('/', groceryListCtrl.create);
-  router.get('/show', groceryListCtrl.show);
-  router.put('/:id', groceryListCtrl.update)
-  router.put('/:id/grocerylistedit', groceryListCtrl.updateGroceryList)
+router.post("/", groceryListCtrl.create);
+router.get("/show", groceryListCtrl.show);
+router.put("/:id", groceryListCtrl.update);
+router.put("/:id/grocerylistedit", groceryListCtrl.updateGroceryList);
 
+router.delete("/removegrocery/:id/:idx", groceryListCtrl.removegrocery);
 
+router.delete("/groceries/:id", groceryListCtrl.delete);
 
-  router.delete('/removegrocery/:id/:idx', groceryListCtrl.removegrocery);
-  
-  router.delete('/groceries/:id', groceryListCtrl.delete);
-  
-  router.post('/addgrocery', groceryListCtrl.addGrocery);
-  
-  router.get('/creategrocery', groceryListCtrl.createGrocery);
+router.post("/addgrocery", groceryListCtrl.addGrocery);
 
-  
+router.get("/creategrocery", groceryListCtrl.createGrocery);
 
-  router.post('/:id/grocery', groceryListCtrl.grocery);
-  router.get('/lists/:id', groceryListCtrl.groceryList);
-  
-  
-  module.exports = router;
+router.post("/:id/grocery", groceryListCtrl.grocery);
+router.get("/lists/:id", groceryListCtrl.groceryList);
+
+router.get("/:id", groceryListCtrl.groceryList);
+
+module.exports = router;
